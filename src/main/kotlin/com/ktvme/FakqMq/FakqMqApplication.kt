072a8d80db1b78215d4498ef0609fe71e7024d0b
Fanwu <fanwu@star-net.cn>
2025-03-27 16:33:47 +0800

    init import

---
+++ src/main/kotlin/com/ktvme/FakqMq/FakqMqApplication.kt
@@ -0,0 +1,11 @@
+package com.ktvme.FakqMq
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FakqMqApplication
+
+fun main(args: Array<String>) {
+	runApplication<FakqMqApplication>(*args)
+}
---
+++ src/test/kotlin/com/ktvme/FakqMq/FakqMqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ktvme.FakqMq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FakqMqApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
